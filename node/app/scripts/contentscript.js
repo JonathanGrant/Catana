57aac38d171233d9e41df984c402dcd3d261601a
Blake Macnair <blakemacnair@gmail.com>
2016-10-01 14:47:05 -0400

    Fuck merge conflicts again

--- node/app/scripts/contentscript.js
+++ node/app/scripts/contentscript.js
@@ -1,36 +1,5 @@
 "use strict";
 
-function getUserScore(userId) {
-  var requestData = { "operation": "read", "tableName": "AddMeUsers", "payload": { "Key": { "userid": userId } } };
-  $.ajax({
-    url: "https://rdsmefueg6.execute-api.us-east-1.amazonaws.com/prod",
-    type: "POST",
-    dataType: "json",
-    data: JSON.stringify(requestData),
-    beforeSend: function beforeSend(xhr) {
-      xhr.setRequestHeader("x-api-key", awsKey);
-    },
-    success: function success(data, status, jq) {
-      return data["Item"].bullyscore;
-    }
-  });
-}
-
-function updateAWS(newScore, userId) {
-  var requestData = { "operation": "update", "tableName": "AddMeUsers", "payload": { "Key": { "userid": userId }, "UpdateExpression": "set bullyscore = :ns", "ExpressionAttributeValues": { ":ns": newScore } } };
-  $.ajax({
-    url: "https://rdsmefueg6.execute-api.us-east-1.amazonaws.com/prod",
-    type: "POST",
-    dataType: "json",
-    data: JSON.stringify(requestData),
-    beforeSend: function beforeSend(xhr) {
-      xhr.setRequestHeader("x-api-key", awsKey);
-    },
-    success: function success(data, status, jq) {
-      console.log(data, status, jq);
-    }
-  });
-}
 
 function watson(phrase, node) {
   var myUrl = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=" + phrase;
@@ -56,8 +25,6 @@ function watson(phrase, node) {
 function watsonCallback(score, node) {
   if (score * 100 > 50) {
     // console.log(score);
-    console.log(updateAWS("420", "JonnyIsCoo1"));
-    getUserScore("JonnyIsCoo1");
     var myCat = document.createElement("IMG");
     var height = Math.round(Math.random() * 300) + 100;
     var width = Math.round(Math.random() * 200) + 100;
@@ -103,6 +70,7 @@ function run() {
   var textNodes = textNodesUnder(document.body);
   textNodes.forEach(function (node) {
     var lower = node.textContent.toLowerCase();
+    var elem = document.activeElement
     if (!isInElement(node, elem) && (lower.includes("fuck") || lower.includes("shit") || lower.includes("bitch") || lower.includes("piss"))) {
       return watson(lower, node);
     }
