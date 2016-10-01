b0a32e3aac20464d0e9e1ae7163b6d70a607f79c
JonathanGrant <jonathanallengrant@gmail.com>
2016-10-01 09:42:18 -0700

    rest of the extension

--- node/app/scripts.babel/contentscript.js
+++ node/app/scripts.babel/contentscript.js
@@ -1,76 +1,36 @@
-'use strict';
+"use strict";
 
-// require.config({
-//   baseUrl: "scripts"
-// });
-// var request = require('request');
+function getUserScore(userId) {
+  var requestData = {"operation": "read","tableName": "AddMeUsers","payload": {"Key": {"userid": userId}}};
+  $.ajax({
+    url : "https://rdsmefueg6.execute-api.us-east-1.amazonaws.com/prod",
+    type: "POST",
+    dataType : "json",
+    data: JSON.stringify(requestData),
+    beforeSend: function(xhr){xhr.setRequestHeader("x-api-key", awsKey);},  
+    success: function(data, status, jq) {
+      return data["Item"].bullyscore;
+    }
+  });
+}
 
-// function getUserScore(userId) {
-//   var requestData = {
-//     'operation': 'read',
-//     'tableName': 'AddMeUsers',
-//     'payload': {
-//       'Key': {
-//           'userid': userId
-//       }
-//     }
-//   }
-//   request({
-//     url: 'https://rdsmefueg6.execute-api.us-east-1.amazonaws.com/prod',
-//     method: 'POST',
-//     json: true,
-//     headers: {
-//         'content-type': 'application/json',
-//         'x-api-key': process.env.API_KEY,
-//     },
-//     body: requestData
-//   }, function (error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//       console.log('200: ', body)
-//     }
-//     else {
-//       console.log('error: ' + error)
-//       console.log('response.statusCode: ' + response.statusCode)
-//       console.log('response.statusText: ' + response.statusText)
-//     }
-//   });
-// }
+function updateAWS(newScore, userId) {
+  var requestData = {"operation": "update","tableName": "AddMeUsers","payload": {"Key": {"userid": userId},"UpdateExpression": "set bullyscore = :ns","ExpressionAttributeValues": {":ns": newScore}}};
+  $.ajax({
+    url : "https://rdsmefueg6.execute-api.us-east-1.amazonaws.com/prod",
+    type: "POST",
+    dataType : "json",
+    data: JSON.stringify(requestData),
+    beforeSend: function(xhr){xhr.setRequestHeader("x-api-key", awsKey);},  
+    success: function(data, status, jq) {
+      console.log(data, status, jq);
+    }
+  });
+}
 
-// function updateAWS(newScore, userId) {
-//   var requestData = {
-//     'operation': 'update',
-//     'tableName': 'AddMeUsers',
-//     'payload': {
-//       'Key': {
-//           'userid': '6093044061'
-//       },
-//       'UpdateExpression': 'set igid = :id, ig_access = :ac',
-//       'ExpressionAttributeValues': {
-//         ':id': jsonBody.user.id,
-//         ':ac': jsonBody.access_token
-//       }
-//     }
-//   }
-//   request({
-//     url: 'https://rdsmefueg6.execute-api.us-east-1.amazonaws.com/prod',
-//     method: 'POST',
-//     json: true,
-//     headers: {
-//         'content-type': 'application/json',
-//         'x-api-key': process.env.API_KEY,
-//     },
-//     body: requestData
-//   }, function (error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//       console.log('200: ', body)
-//     }
-//     else {
-//       console.log('error: ' + error)
-//       console.log('response.statusCode: ' + response.statusCode)
-//       console.log('response.statusText: ' + response.statusText)
-//     }
-//   });
-// }
+function watson(phrase) {
+  var url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3";
+}
 
 function textNodesUnder(element) {
   let node;
@@ -90,28 +50,29 @@ function textNodesUnder(element) {
 }
 
 function run() {
-  // getUserScore('6507993840');
   let textNodes = textNodesUnder(document.body);
   textNodes.forEach(function(node) {
     var lower = node.textContent.toLowerCase();
-    if (lower.includes('fuck') || lower.includes('shit')) {
-      var myCat = document.createElement('IMG');
+    if (lower.includes("fuck") || lower.includes("shit") || lower.includes("bitch") || lower.includes("piss")) {
+      console.log(updateAWS("420", "JonnyIsCoo1"));
+      getUserScore("JonnyIsCoo1");
+      var myCat = document.createElement("IMG");
       var height = Math.round(Math.random()*300) + 100;
       var width = Math.round(Math.random()*200) + 100;
-      myCat.src = 'http://placekitten.com/' + width + '/' + height;
+      myCat.src = "http://placekitten.com/" + width + "/" + height;
       node.parentElement.appendChild(myCat)
       node.nodeValue = node.
                          nodeValue.
-                         replace(/fuck/g, 'love').
-                         replace(/Fuck/g, 'Love').
-                         replace(/shit/g, 'amazing').
-                         replace(/Shit/g, 'Amazing').
-                         replace(/bitch/g, 'beautiful person').
-                         replace(/Bitch/g, 'Beautiful person').
-                         replace(/piss off/g, 'thank you').
-                         replace(/Piss off/g, 'Thank you').
-                         replace(/piss/g, 'great').
-                         replace(/Piss/g, 'Great')
+                         replace(/fuck/g, "love").
+                         replace(/Fuck/g, "Love").
+                         replace(/shit/g, "amazing").
+                         replace(/Shit/g, "Amazing").
+                         replace(/bitch/g, "beautiful person").
+                         replace(/Bitch/g, "Beautiful person").
+                         replace(/piss off/g, "thank you").
+                         replace(/Piss off/g, "Thank you").
+                         replace(/piss/g, "great").
+                         replace(/Piss/g, "Great")
       }
       return node;
   });
