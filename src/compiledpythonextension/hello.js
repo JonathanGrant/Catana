631a7acde6e6d1da3fecedb0b172755aac53d613
JonathanGrant <jonathanallengrant@gmail.com>
2016-09-30 17:28:58 -0700

    added the shtuff

---
+++ src/compiledpythonextension/hello.js
@@ -0,0 +1,8 @@
+
+var __name__ = "__main__";
+function doHelloMessage() {
+    alert("hello world");
+}
+doHelloMessage();
+document.getElementById("result").innerHTML = "Compiled Python script in Chrome";
+console.log("hello from python");
